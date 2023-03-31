36d4c856549f6b01e3f5d7aff6c2baf6eb41a8cd
Daniel Wagenius <wagenius.daniel@gmail.com>
2023-03-31 09:40:56 +0200

    En sista liten uppdatering med felhantering

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
 Använder unistd.h som enda icke helt 100% standardbibliotek (tror jag).
-Hade en funktion för delay men tydligen gör den funktionen att processorn arbetar hela tiden,
+Hade en funktion för delay som använder clock() men tydligen gör den funktionen att processorn arbetar hela tiden,
 och det är nog inte helt bra för lite äldre datorer. Gick därför tillbaka till sleep() från unistd.h som
 jag använder två gånger i doorstatus.c för att lampan ska bli grön eller röd.
 
@@ -26,6 +26,10 @@ int main()
     {
         fclose(temp);
         temp = fopen("cards.crd", "wb");
+    }
+    if(temp == NULL)
+    {
+        printf("File error, exiting program.\n");
     }
     fclose(temp);
     mainMenu();
